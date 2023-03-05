4b96f9f73d2a7ed64b71eb5d164f46cbc59b5a53
Sharat Jacob <sharatjacob2@gmail.com>
2023-03-05 06:10:59 +0530

    added some interactivity and a footer

--- src/Start.js
+++ src/Start.js
@@ -1,9 +1,11 @@
-import Vector from './Vector.svg';
+import Vector1 from './Vector1.svg';
 import Vector2 from './Vector2.svg';
 import Vector3 from './Vector3.svg';
+import WebsiteVector1 from './WebsiteVector1.svg';
+import WebsiteVector2 from './WebsiteVector2.svg';
+import WebsiteVector3 from './WebsiteVector3.svg';
 import interests from './interests.svg';
 
-import './circle.js';
 
 function Start() {
     return (
@@ -18,14 +20,18 @@ function Start() {
                 enthusiastic about building
                 products that make a world of difference.</p>
 
-        
+
             <div className="Circle interestcircle"></div>
             <div className="subheadsquare"></div>
             <p class="heading">INTERESTS</p>
             <p class="subheading">what I'm into, lol</p>
-            <img src={Vector2} alt="prop" className="Vectors" style={{top:'476.22px'}}/>
-            <img src={Vector3} alt="prop" className="Vectors" style={{top:'456.22px'}}/>
-            <img src={Vector} alt="prop" className="Vectors" style={{top:'436.22px'}}/>
+            <img src={Vector2} alt="prop" className="Vectors" style={{ top: '476.22px' }} />
+            <img src={Vector3} alt="prop" className="Vectors" style={{ top: '456.22px' }} />
+            <img src={Vector1} alt="prop" className="Vectors" style={{ top: '436.22px' }} />
+
+            <div class="decor">
+                <div className="decorstuff"></div>
+            </div>
 
             <div className="pendulum3 pendulums"></div>
             <div className="pendulum2 pendulums"></div>
@@ -33,14 +39,75 @@ function Start() {
 
             <img src={interests} alt="prop" className="interests" />
 
+            <img src={WebsiteVector3} alt="prop" className="Vectors" style={{ top: '2062px' }} />
+            <img src={WebsiteVector2} alt="prop" className="Vectors" style={{ top: '2042px' }} />
+            <img src={WebsiteVector1} alt="prop" className="Vectors" style={{ top: '2012px' }} />
 
-            <div class="circle"></div>
+            <div className='websiteholder'></div>
+            <div className='websitelinkholder'></div>
+            <p className='websiteintro'>For your perusal, my collection of short stories and blog posts over the years</p>
+            <a className='websitelink' href="https://www.sharatjacob2.github.io">sharatjacob2.github.io</a>
 
 
-            <div className="Circle" style={{ width: '230.87px', height: '230.87px', top: '1728.62px', background: '#920D0D' }}></div>
+
+
+
+
+            <div className='footer'>
+                <div className='Circle finalelp1'></div>
+                <div className='Circle finalelp2'></div>
+                <div className='Circle finalelp3'></div>
+                <p className='footertext'>CIRCA SHARAT 2023</p>
+            </div>
+
         </div>
     )
 
 }
+
+const circle1 = document.querySelector('.pendulum1');
+const circle2 = document.querySelector('.pendulum2');
+const circle3 = document.querySelector('.pendulum3');
+const circle4 = document.querySelector('.decorstuff');
+const box = document.querySelector('.interests');
+// const teller = document.querySelector('.websiteholder');
+
+
+let lastScrollY = window.scrollY;
+
+function handleScroll() {
+    const scrollY = window.scrollY;
+    if (scrollY > lastScrollY) {
+        box.style.transform = `translateX(-50px)`;
+        circle1.style.transform = 'rotate(-60deg) translateX(10px) translateY(450px) ';
+        circle2.style.transform = 'rotate(-60deg) translateX(310px) translateY(500px) ';
+        circle3.style.transform = 'rotate(-60deg) translateX(560px) translateY(550px) ';
+        circle4.style.offsetDistance = '100%';
+
+    } else if (scrollY < lastScrollY) {
+        circle1.style.transform = 'rotate(60deg) translateX(-250px) translateY(-250px) ';
+        circle2.style.transform = 'rotate(60deg) translateX(-250px) translateY(-250px) ';
+        circle3.style.transform = 'rotate(60deg) translateX(-250px) translateY(-250px) ';
+        circle4.style.offsetDistance = '0%';
+        box.style.transform = `translateX(50px)`;
+    }
+    // if ((isElementInViewport(teller))) {
+    //     circle1.style.transform = 'translateY(350px) scale(0.5)';
+    // }
+    lastScrollY = scrollY;
+}
+
+window.addEventListener('scroll', handleScroll);
+
+// function isElementInViewport(el) {
+//     const rect = el.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
+
 
 export default Start;
